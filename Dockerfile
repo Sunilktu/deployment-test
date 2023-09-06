850f36c3ede5a1bbf1dbf6ea062e38b04457065a
Sunil <turpati.sunil@gmail.com>
2023-09-06 16:20:47 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+
+# Use the official Python image as the base image
+FROM python:3.9
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install any dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Expose the port that the Flask application will run on
+EXPOSE 5000
+
+# Define the command to run the Flask application
+CMD ["python", "calculator.py"]
---
+++ calculator.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('calculator.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    try:
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operator = request.form['operator']
+
+        if operator == 'add':
+            result = num1 + num2
+        elif operator == 'subtract':
+            result = num1 - num2
+        elif operator == 'multiply':
+            result = num1 * num2
+        elif operator == 'divide':
+            if num2 == 0:
+                return "Division by zero is not allowed."
+            result = num1 / num2
+        else:
+            return "Invalid operator"
+
+        return f"Result: {result}"
+
+    except ValueError:
+        return "Invalid input. Please enter valid numbers."
+
+if __name__ == '__main__':
+    app.run(debug=True)
